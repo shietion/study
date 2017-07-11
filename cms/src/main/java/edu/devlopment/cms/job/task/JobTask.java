5d6231cebaf71407c98b435caf6679ba313a3fa2
dujianqiao <dujianqiao@zhihuisystem.com>
2017-07-11 16:48:16 +0800

    修改代码

--- cms/src/main/java/edu/devlopment/cms/job/task/JobTask.java
+++ cms/src/main/java/edu/devlopment/cms/job/task/JobTask.java
@@ -2,7 +2,6 @@ package edu.devlopment.cms.job.task;
 
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
-import cn.edu.hfut.dmic.webcollector.crawler.lagou.JobCrawler;
 
 /**
  * 职位定时器
@@ -19,8 +18,8 @@ public class JobTask {
     @Scheduled(cron = "0 0/2 * * * ? ")
     public void taskCycle() {
 	System.out.println("定时任务================================");
-	JobCrawler job = new JobCrawler();
-	job.crawler();
+//	JobCrawler job = new JobCrawler();
+//	job.crawler();
     }
 
 }
